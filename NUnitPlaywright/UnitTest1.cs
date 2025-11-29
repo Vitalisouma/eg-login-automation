aa36be8147e4f379d83547d91ab0b735c6ab5c1b
Vitalis Atonga <vitalis.ouma@yahoo.co.uk>
2025-11-29 09:46:06 +0300

    Add project files.

---
+++ NUnitPlaywright/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using NUnitPlaywright.Pages;
+
+namespace NUnitPlaywright
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class Tests : PageTest
+    {
+        [Test]
+        public async Task SuccessfulLoginTest()
+        {
+            var loginPage = new LoginPage(Page); // Use the provided 'Page' property
+
+            await loginPage.GotoAsync();
+            await loginPage.RegisterAsync("John Smith", "ThisShouldBeEmail@yahoo.co.uk");
+            await Expect(Page).ToHaveTitleAsync("Login Page");
+            await loginPage.LoginAsync("RegisteredEmail@yahoo.co.uk", "Boxing2012@");
+            await loginPage.LoginSuccessAsync();
+            await Expect(Page.Locator("#errorMsgMail")).ToHaveTextAsync("Valid Email"); //or>  await Expect(Page.GetByText("Valid Email", new() { Exact = true })).ToBeVisibleAsync();
+            await Expect(Page.GetByText("Valid Password")).ToBeVisibleAsync(); //or> await Expect(Page.Locator("#errorMsgPwd")).ToHaveTextAsync("Valid Password");
+        }
+
+        [Test]
+        public async Task ShowHidePassword()
+        {
+            var loginPage = new LoginPage(Page); // Use the provided 'Page' property
+
+            await loginPage.GotoAsync();
+            await loginPage.RegisterAsync("John Smith", "ThisShouldBeEmail@yahoo.co.uk");
+            await Expect(Page).ToHaveTitleAsync("Login Page");
+            await loginPage.ShowHidePasswordAsync("RegisteredEmail@yahoo.co.uk", "Boxing2012@");
+
+            await Expect(Page.Locator("#password")).ToHaveValueAsync("Boxing2012@");
+            // 
+        }
+    }
+}
